26282cda5579593890144635228d2194293f8bac
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-26 16:36:03 -0300

    Remove: password do retorno de criação do usuário (POST)

--- src/app/service/user.ts
+++ src/app/service/user.ts
@@ -4,14 +4,10 @@ import UserModel from '../model/user';
 import UserUtilities from '../utilities/user';
 import ERRORS from '../utilities/errors';
 
-const create = async (payload: User): Promise<User> => {
-  const isAlreadyCreated = await UserModel.findOne({
-    $or: [
-      { email: payload.email },
-      { restaurantId: payload.restaurantId },
-    ],
-  });
-
+const create = async (payload: User): Promise<Partial<User>> => {
+  const isAlreadyCreated = await UserModel.findOne(
+    { email: payload.email },
+  );
   if (isAlreadyCreated) {
     throw ERRORS.USER.ALREADY_EXISTS;
   }
@@ -28,7 +24,10 @@ const create = async (payload: User): Promise<User> => {
     ...payload,
     password: UserUtilities.hashPassword(payload.password) },
   );
-  return response.toObject();
+  const user: Partial<User> = response.toObject();
+  delete user.password;
+
+  return user;
 };
 
 const login = async (payload: Partial<User>): Promise<void> => {
